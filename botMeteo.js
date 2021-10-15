b37288f358b7486c644bf3687b1a9691de9ecabb
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2021-10-15 08:44:55 +0200

    pour récupération et voir erreurs multiples ensemble

--- botMeteo.js
+++ botMeteo.js
@@ -1,18 +1,60 @@
+import * as weather from "openweather-apis";
+import discord  from 'discord.js';
+
+const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+//const { prefix, token, apitoken } = import ('./config.json');
+//const axios = require('axios')
+
 export const ready =()=> {
     console.log("Ready !");
 };
 
 const prefixCmd = '!';
 
-export const meteo = (msg) => {
+//export const axios = (msg) => {
 //L'appel de la météo doit être préfixé
-    if(!msg.content.startsWith(prefixCmd) || msg.author.bot) return
+  //  if(false){
+  //  }else{
+  //      axios
+  //          .get(
+  //              'http://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=${apitoken}'
+  //             )
+  //          .then(response => {
+  //               let apiData = response;
+  //               let currentTemp = Math.ceil(apiData.data.main.temp)
+  //               let maxTemp = apiData.data.main.temp_max;
+  //               let minTemp = apiData.data.main.temp_min;
+  //               let humidity = apiData.data.main.humidity;
+  //               let wind = apiData.data.wind.speed;
+  //               let author = message.author.username
+  //               let profile = message.author.displayAvatarURL
+  //               // let icon = apiData.data.weather[0].icon
+  //               let cityName = args
+  //               let country = apiData.data.sys.country
+  //               let pressure = apiData.data.main.pressure;
+  //             let cloudness = apiData.data.weather[0].description;
+  //              message.channel.send(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country));
+  //          }).catch(err => {
+  //          message.reply(`Enter a vailid city name`)
+  //      })
+  //  }
 //Peu importe la longueur du message, on le préfixe
-     const args = msg.content.slice(prefixCmd.length).trim().split(/ +/);
-     const command = args.shift().toLowerCase();
-
-     if (command === "weather") {
-         msg.reply("Le temps d'aujourd'hui !");
-     }
-};
+//      const args = msg.content.slice(prefixCmd.length).trim().split(/ +/);
+//      const command = args.shift().toLowerCase();
+//
+//      if (command === "weather") {
+//          msg.reply("Le temps d'aujourd'hui !");
+//      }
+// };
+/**
+ *
+ * @param {string} ville
+ */
+export const meteo = (ville)=>{
+    weather.setCity(ville);
+    weather.getDescription(function(err, response){
+        if(err) console.error(err);
+        console.log(response);
+    });
+}
 
--- main.js
+++ main.js
@@ -1,11 +1,52 @@
 import { Client, Intents } from 'discord.js';
-import {meteo, ready} from './botMeteo.js';
-const token =process.env.DISCORD_TOKEN;
+import { meteo, ready} from './botMeteo.js';
+import * as weather from 'openweather-apis';
+import {commands} from './commands.json';
 
+const token =process.env.DISCORD_TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 
 client.login(token);
 
+function updateCommand(server) {
+    const {REST} = require('@discordjs/rest');
+    const {Routes} = require('discord-api-types/v9');
+    const rest = new REST({version: '9'}).setToken(token);
+
+    rest.put(Routes.applicationGuildCommands(client.user.id, server.id), { body: commands}).catch(console.error);
+}
+
+client.on('ready', function(){
+    client.guilds.fetch().then(function(servers){
+        server.forEach(function(server){
+            updateCommand(server);
+        })
+    })
+});
+
+const tokenAPI = process.env.API_WEATHER_TOKEN;
+weather.setAPPID(tokenAPI);
+weather.setLang("FR");
+weather.setUnits("Metric");
+
 ready();
 meteo();
+
+// import { Client, Intents } from 'discord.js';
+// import {meteo, ready} from './botMeteo.js';
+// import * as weather from 'openweather-apis';
+//
+// const token =process.env.DISCORD_TOKEN;
+//
+// const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+//
+// client.login(token);
+//
+// const tokenAPI = process.env.API_WEATHER_TOKEN;
+// weather.setAPPID(tokenAPI);
+// weather.setLang("FR");
+// weather.setUnits("Metric");
+//
+// ready();
+// meteo("Nantes");
